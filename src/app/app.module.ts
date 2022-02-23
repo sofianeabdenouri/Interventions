8dfc605b5cfc922632d5c310751c66d269e00b04
sofianeabdenouri <sofianeabdenouri@gmail.com>
2022-02-23 14:23:37 -0500

    test 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProblemeComponent } from './probleme/probleme.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProblemeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/probleme/probleme.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'Inter-probleme',
+  templateUrl: './probleme.component.html',
+  styleUrls: ['./probleme.component.css']
+})
+export class ProblemeComponent implements OnInit {
+
+  problemeForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+  this.problemeForm = this.fb.group({
+    prenom: ['',[Validators.minLength(3)]]
+  });
+  }
+  
+  save(): void {
+  }
+
+
+}
