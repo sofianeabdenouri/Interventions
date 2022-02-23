62dd29a0a61f38434de4c90c62a6c51391ec0dc9
sofianeabdenouri <sofianeabdenouri@gmail.com>
2022-02-23 14:28:03 -0500

    test 2

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -31,4 +31,11 @@ describe('ProblemeComponent', () => {
     let errors = zone.errors || {};
     expect(errors['minlength']).toBeTruthy();
     });
+    it("#2 | Zone PRÉNOM valide avec 3 caractères", () =>{
+      let zone = component.problemeForm.controls['prenom'];
+      zone.setValue("a".repeat(3));
+      let errors = zone.errors || {};
+      expect(errors['minlength']).toBeFalsy();
+      });
+      
 });
